ac175891e36751c82b76f714d54138061317471f
Vinicius Gabriel <viniciusrichard98@gmail.com>
2023-11-17 15:06:15 -0300

    adicionando pagina do pokemon

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let pokedexSpace = document.getElementById('pokedexSpace');
 let button = document.getElementById('add_button');
 let poke = document.getElementsByClassName('pokemon');
 let themeImg = document.getElementById('theme-img');
-let bg = document.getElementById('bg')
+let bg = document.getElementById('bg');
+let pokeData = document.getElementById('pokemon-data');
+let buttonDiv = document.getElementById('button-div');
+let pokeImg = document.getElementById('pokeStatus__img');
+let pokeName = document.getElementById('pokeStatus__name');
 
 async function pokedex() {
     let conexao = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${a}&limit=${b}`)
@@ -17,23 +21,13 @@ async function pokedex() {
 
 
             pokedexSpace.innerHTML += `
-                <div class="pokemon" style="background-color: blue">
+                <div class="pokemon" style="background-color: blue" onclick="pokemonStatus(this)" id="${pokemon.name}">
                 <img src='${conexaoConvertida1.sprites.front_default}' class="poke_img"/>
                 <br><p>${pokemon.name}</p>
                 </div>
                  `
         }
         pokedex1(pokemon)
-
-        async function conexao2() {
-            let conexao2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}
-            `)
-            let conexao22 = await conexao2.json();
-
-            console.log(conexao22.types);
-        }
-
-        conexao2();
     });
 }
 
@@ -57,4 +51,28 @@ function tema() {
         bg.setAttribute('src', "./assets/imagens/uma-paisagem-montanhosa-com-um-coracao-amarelo-no-topo.jpg")
     }
 
+}
+
+function pokemonStatus(obj) {
+    pokedexSpace.style.display = "none";
+    buttonDiv.style.display = "none";
+    pokeData.style.display = "block";
+    async function conexao2() {
+        let conexao2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${obj.id}
+        `)
+        let conexao22 = await conexao2.json();
+    
+        console.log(conexao22);
+
+        pokeImg.setAttribute('src', `${conexao22.sprites.front_default}`)
+        pokeName.innerText = `${conexao22.name}`
+    }
+    conexao2();
+}
+
+function back(){
+    pokedexSpace.style.display = "flex";
+    buttonDiv.style.display = "block";
+    pokeData.style.display = "none";
+    pokeImg.setAttribute('src', "")
 }
